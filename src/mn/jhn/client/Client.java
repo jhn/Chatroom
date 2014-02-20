8bea418aae26b92d8692c4bdb1cd83a763410726
Johan Mena <jmm2371@columbia.edu>
2014-02-19 20:45:27 -0500

    Basic client

--- src/mn/jhn/client/Client.java
+++ src/mn/jhn/client/Client.java
@@ -1,5 +1,32 @@
 package mn.jhn.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client
 {
+    public static void main(String[] args) throws IOException
+    {
+        String host = args[0];
+        int port    = Integer.parseInt(args[1]);
+
+        Socket socket = new Socket(host, port);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+        String userInput;
+        while (true)
+        {
+            userInput = stdIn.readLine();
+            if (userInput != null)
+            {
+                out.println(userInput);
+                System.out.println(in.readLine());
+            }
+        }
+    }
 }
